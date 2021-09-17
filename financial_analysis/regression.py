33d6982be6b065035b78c89214d1bb9701733ef6
Dennis Wu <dennis810411@gmail.com>
2021-09-18 01:44:36 +0800

    linearRegression

---
+++ financial_analysis/regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+
+X, y = make_regression(n_samples=100,n_features=1,noise=20)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+regr = linear_model.LinearRegression()
+regr.fit(X_train, y_train)
+
+w_0 = regr.intercept_
+w_1 = regr.coef_
+
+score_train = regr.score(X_train, y_train)
+score_test = regr.score(X_test, y_test)
+
+print(w_0, w_1, score_train, score_test)
