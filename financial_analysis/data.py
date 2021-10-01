4422a25ef60fb150b3f57a9f7d870af00d0a834e
Dennis Wu <dennis810411@gmail.com>
2021-10-01 22:34:53 +0800

    read data and function params and std

--- financial_analysis/data.py
+++ financial_analysis/data.py
@@ -1,9 +1,20 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+import pandas as pd
+from pandas_ods_reader import read_ods
 
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)  # 實作上加入 random_state=42 固定結果
 
-print(iris.keys()) # 資料為 dictionary array,可用pandas針對字典Keys內容預處理
+
+# print(iris.keys()) # 資料為 dictionary array,可用pandas針對字典Keys內容預處理
+
+
+def read_data(symbol='spx', style='csv'):
+    if style == 'ods':
+        df = read_ods(f'{symbol}.ods')
+    else:
+        df = pd.read_csv(f'{symbol}.csv')
+    return df
--- financial_analysis/return_analysis.py
+++ financial_analysis/return_analysis.py
@@ -1,36 +1,48 @@
 import pandas as pd
-from pandas_ods_reader import read_ods
+from financial_analysis import data
+import matplotlib.pyplot as plt
 
 
-#close_to_daily_return
-def daily_return(symbol='spx', style='csv'):
-    #注意資料為順時間排序
-    if style == 'ods':
-        df=read_ods(f'{symbol}.ods')
-    else:
-        df=pd.read_csv(f'{symbol}.csv')
+# close_to_daily_return (確認資料為順時間排序)
+def daily_return(df, start=None, end=None):
     df.set_index(pd.to_datetime(df['Date']), inplace=True)
     df = df.sort_index()
     df.drop('Date', axis=1, inplace=True)
-    dr=df.pct_change(1)
+    df = df.loc[start:end]
+    dr = df.pct_change(1)
+    dr = dr.dropna()
     return dr
 
 
-#daily_return_to_mdd
+# daily_return_to_std (確認資料為順時間排序)
+def r_std(dr, period=0):
+    period_dr = dr.iloc[-period:-1]
+    daily_std = period_dr.std()
+    annual_std = daily_std * 252 ** 0.5
+    rolling_annual_std = dr.rolling(252).std() * 252 ** 0.5
+    rolling_annual_std = rolling_annual_std.dropna()
+    result = {'daily_std': daily_std[0], 'annual_std': annual_std[0], 'rolling_annual_std': rolling_annual_std}
+    return result
+
+
+# daily_return_to_mdd (確認資料為順時間排序)
 def mdd(dr):
-    # 注意資料為順時間排序
     r = dr.add(1).cumprod()
     dd = r.div(r.cummax()).sub(1)
     mdd = dd.min()
     end = dd.idxmin()
     start = r.loc[:end[0]].idxmax()
     days = end - start
-    result = f'最大跌幅 {mdd[0]*100:3.2f}%, 起跌日 {start[0]:%Y-%m-%d}, 止跌日 {end[0]:%Y-%m-%d}, 下跌時間 {days[0]}'
+    result = {'mdd': mdd[0], 'start': start[0], 'end': end[0], 'days': days[0]}
     return result
 
-spx = daily_return('spx','ods')
+
+spx = daily_return(data.read_data('spx', 'ods'))
 spx_mdd = mdd(spx)
-print(spx)
-print(spx_mdd)
+spx_std = r_std(spx)
+print(f'最大跌幅 {spx_mdd["mdd"]*100:3.2f}%, 起跌日 {spx_mdd["start"]:%Y-%m-%d}, 止跌日 {spx_mdd["end"]:%Y-%m-%d}, '
+      f'下跌時間 {spx_mdd["days"]}')
+print(f'日波動 {spx_std["daily_std"]*100:3.2f}%, 年化波動 {spx_std["annual_std"]*100:3.2f}%')
 
-
+# plt.plot(spx_std['rolling_annual_std'])
+# plt.show()
